01e8a12f3015515a3f29bb7552976627909df524
Anna <anna.afonina.04@list.ru>
2024-10-16 15:34:53 +0500

    new

---
+++ pythonProject2/lib.py
@@ -0,0 +1,7 @@
+def count_common_elements(*lists):
+    if not lists:
+        return 0
+    common_elements = set(lists[0])
+    for lst in lists[1:]:
+        common_elements.intersection_update(lst)
+    return len(common_elements)
